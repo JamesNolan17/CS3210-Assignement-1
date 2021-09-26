56ca482ae39eeed632fdd7d28d9548d66dded715
James Nolan <vip17jamesnolan@gmail.com>
2021-09-27 01:27:03 +0800

    update fuck

--- Makefile
+++ Makefile
@@ -1,6 +1,6 @@
 build:
 	gcc sb/sb.c util.c exporter.c goi.c main.c -o goi.out
-	gcc -g sb/sb.c util.c exporter.c goi_pthread.c main_pthread.c -o goi_threads.out
+	gcc -lpthread -g sb/sb.c util.c exporter.c goi_pthread.c main_pthread.c -o goi_threads.out
 	#gcc -Xpreprocessor -fopenmp sb/sb.c util.c exporter.c goi_omp.c main_omp.c -o goi_omp.out -lomp
 	gcc -fopenmp sb/sb.c util.c exporter.c goi_omp.c main_omp.c -o goi_omp.out
 clean:
--- goi_pthread.c
+++ goi_pthread.c
@@ -24,16 +24,14 @@
 /**
  * Specifies the number(s) of live neighbors of the same faction required for a dead cell to become alive.
  */
-bool isBirthable(int n)
-{
+bool isBirthable(int n) {
     return n == 3;
 }
 
 /**
  * Specifies the number(s) of live neighbors of the same faction required for a live cell to remain alive.
  */
-bool isSurvivable(int n)
-{
+bool isSurvivable(int n) {
     return n == 2 || n == 3;
 }
 
@@ -51,8 +49,8 @@ bool willFight(int n) {
  * invaders can be NULL if there are no invaders.
  */
 
-int getNextState(const int *currWorld, const int *invaders, int nRows, int nCols, int row, int col, bool *diedDueToFighting)
-{
+int getNextState(const int *currWorld, const int *invaders, int nRows, int nCols, int row, int col,
+                 bool *diedDueToFighting) {
     // we'll explicitly set if it was death due to fighting
     *diedDueToFighting = false;
 
@@ -60,8 +58,7 @@ int getNextState(const int *currWorld, const int *invaders, int nRows, int nCols
     int cellFaction = getValueAt(currWorld, nRows, nCols, row, col);
 
     // did someone just get landed on?
-    if (invaders != NULL && getValueAt(invaders, nRows, nCols, row, col) != DEAD_FACTION)
-    {
+    if (invaders != NULL && getValueAt(invaders, nRows, nCols, row, col) != DEAD_FACTION) {
         *diedDueToFighting = cellFaction != DEAD_FACTION;
         return getValueAt(invaders, nRows, nCols, row, col);
     }
@@ -71,13 +68,10 @@ int getNextState(const int *currWorld, const int *invaders, int nRows, int nCols
     memset(neighborCounts, 0, MAX_FACTIONS * sizeof(int));
 
     // count neighbors (and self)
-    for (int dy = -1; dy <= 1; dy++)
-    {
-        for (int dx = -1; dx <= 1; dx++)
-        {
+    for (int dy = -1; dy <= 1; dy++) {
+        for (int dx = -1; dx <= 1; dx++) {
             int faction = getValueAt(currWorld, nRows, nCols, row + dy, col + dx);
-            if (faction >= DEAD_FACTION)
-            {
+            if (faction >= DEAD_FACTION) {
                 neighborCounts[faction]++;
             }
         }
@@ -86,8 +80,7 @@ int getNextState(const int *currWorld, const int *invaders, int nRows, int nCols
     // we counted this cell as its "neighbor"; adjust for this
     neighborCounts[cellFaction]--;
 
-    if (cellFaction == DEAD_FACTION)
-    {
+    if (cellFaction == DEAD_FACTION) {
         // this is a dead cell; we need to see if a birth is possible:
         // need exactly 3 of a single faction; we don't care about other factions
 
@@ -95,19 +88,15 @@ int getNextState(const int *currWorld, const int *invaders, int nRows, int nCols
         int newFaction = DEAD_FACTION;
 
         // start at 1 because we ignore dead neighbors
-        for (int faction = DEAD_FACTION + 1; faction < MAX_FACTIONS; faction++)
-        {
+        for (int faction = DEAD_FACTION + 1; faction < MAX_FACTIONS; faction++) {
             int count = neighborCounts[faction];
-            if (isBirthable(count))
-            {
+            if (isBirthable(count)) {
                 newFaction = faction;
             }
         }
 
         return newFaction;
-    }
-    else
-    {
+    } else {
         /**
          * this is a live cell; we follow the usual rules:
          * Death (fighting): > 0 hostile neighbor
@@ -117,24 +106,20 @@ int getNextState(const int *currWorld, const int *invaders, int nRows, int nCols
          */
 
         int hostileCount = 0;
-        for (int faction = DEAD_FACTION + 1; faction < MAX_FACTIONS; faction++)
-        {
-            if (faction == cellFaction)
-            {
+        for (int faction = DEAD_FACTION + 1; faction < MAX_FACTIONS; faction++) {
+            if (faction == cellFaction) {
                 continue;
             }
             hostileCount += neighborCounts[faction];
         }
 
-        if (willFight(hostileCount))
-        {
+        if (willFight(hostileCount)) {
             *diedDueToFighting = true;
             return DEAD_FACTION;
         }
 
         int friendlyCount = neighborCounts[cellFaction];
-        if (!isSurvivable(friendlyCount))
-        {
+        if (!isSurvivable(friendlyCount)) {
             return DEAD_FACTION;
         }
 
@@ -142,20 +127,21 @@ int getNextState(const int *currWorld, const int *invaders, int nRows, int nCols
     }
 }
 
-void *thread_task( void* threadData){
-    struct thread_data *thread_data_ptr = (struct thread_data*) threadData;
-//    printf("%d - ", thread_data_ptr->nRows);
-//    printWorld(thread_data_ptr->currWorld,thread_data_ptr->nRows, thread_data_ptr->nCols );
+void *thread_task(void *threadData) {
+    struct thread_data *thread_data_ptr = (struct thread_data *) threadData;
+
     for (int i = 0; i < thread_data_ptr->numGrid; i++) {
         bool diedDueToFighting;
-       // printf("%d - ", thread_data_ptr->nextstate[i]);
-        thread_data_ptr->nextstate[i] = getNextState(thread_data_ptr->currWorld, thread_data_ptr->invaders, thread_data_ptr->nRows, thread_data_ptr->nCols, thread_data_ptr->row[i],  thread_data_ptr->col[i],  &diedDueToFighting) ;
-        if (diedDueToFighting){
+
+        thread_data_ptr->nextstate[i] = getNextState(thread_data_ptr->currWorld, thread_data_ptr->invaders,
+                                                     thread_data_ptr->nRows, thread_data_ptr->nCols,
+                                                     thread_data_ptr->row[i], thread_data_ptr->col[i],
+                                                     &diedDueToFighting);
+        if (diedDueToFighting) {
             thread_data_ptr->numDeath += 1;
         }
-        //nextState = getNextState(world, inv, nRows, nCols, row, col, &diedDueToFighting);
-        //printf("%d - ", thread_data_ptr->nextstate[i]);
     }
+    exit(0);
 }
 
 /**
@@ -164,8 +150,8 @@ void *thread_task( void* threadData){
  * goi does not own startWorld, invasionTimes or invasionPlans and should not modify or attempt to free them.
  * nThreads is the number of threads to simulate with. It is ignored by the sequential implementation.
  */
-int goi_pthread(int nThreads, int nGenerations, const int *startWorld, int nRows, int nCols, int nInvasions, const int *invasionTimes, int **invasionPlans)
-{
+int goi_pthread(int nThreads, int nGenerations, const int *startWorld, int nRows, int nCols, int nInvasions,
+                const int *invasionTimes, int **invasionPlans) {
     //init the pthreads
     pthread_t threads[nThreads];
     int rc;
@@ -178,15 +164,12 @@ int goi_pthread(int nThreads, int nGenerations, const int *startWorld, int nRows
     // init the world!
     // we make a copy because we do not own startWorld (and will perform free() on world)
     int *world = malloc(sizeof(int) * nRows * nCols);
-    if (world == NULL)
-    {
+    if (world == NULL) {
         return -1;
     }
 
-    for (int row = 0; row < nRows; row++)
-    {
-        for (int col = 0; col < nCols; col++)
-        {
+    for (int row = 0; row < nRows; row++) {
+        for (int col = 0; col < nCols; col++) {
             setValueAt(world, nRows, nCols, row, col, getValueAt(startWorld, nRows, nCols, row, col));
         }
     }
@@ -199,16 +182,14 @@ int goi_pthread(int nThreads, int nGenerations, const int *startWorld, int nRows
 #if EXPORT_GENERATIONS
     exportWorld(world, nRows, nCols);
 #endif
-    for(int t = 0; t < nThreads; t++){
+    for (int t = 0; t < nThreads; t++) {
         threadData[t].numDeath = 0;
         threadData[t].numGrid = 0;
     }
 
-    for (int row = 0; row < nRows; row++)
-    {
-        for (int col = 0; col < nCols; col++)
-        {
-            int thread_num = ( row * nCols + col  ) % nThreads;
+    for (int row = 0; row < nRows; row++) {
+        for (int col = 0; col < nCols; col++) {
+            int thread_num = (row * nCols + col) % nThreads;
             //  printf("thread : %d \n ", thread_num);
             int num = threadData[thread_num].numGrid;
             //  printf("%d -  ", num);
@@ -216,31 +197,27 @@ int goi_pthread(int nThreads, int nGenerations, const int *startWorld, int nRows
             threadData[thread_num].col[num] = col;
             threadData[thread_num].nextstate[num] = 0;
             threadData[thread_num].numDeath = 0;
-            threadData[thread_num].numGrid+=1;
+            threadData[thread_num].numGrid += 1;
 
         }
     }
 
     // Begin simulating
     int invasionIndex = 0;
-    for (int i = 1; i <= nGenerations; i++)
-    {
+    for (int i = 1; i <= nGenerations; i++) {
         // is there an invasion this generation?
         int *inv = NULL;
-        if (invasionIndex < nInvasions && i == invasionTimes[invasionIndex])
-        {
+        if (invasionIndex < nInvasions && i == invasionTimes[invasionIndex]) {
             // we make a copy because we do not own invasionPlans
             inv = malloc(sizeof(int) * nRows * nCols);
-            if (inv == NULL)
-            {
+            if (inv == NULL) {
                 free(world);
                 return -1;
             }
-            for (int row = 0; row < nRows; row++)
-            {
-                for (int col = 0; col < nCols; col++)
-                {
-                    setValueAt(inv, nRows, nCols, row, col, getValueAt(invasionPlans[invasionIndex], nRows, nCols, row, col));
+            for (int row = 0; row < nRows; row++) {
+                for (int col = 0; col < nCols; col++) {
+                    setValueAt(inv, nRows, nCols, row, col,
+                               getValueAt(invasionPlans[invasionIndex], nRows, nCols, row, col));
                 }
             }
             invasionIndex++;
@@ -248,16 +225,14 @@ int goi_pthread(int nThreads, int nGenerations, const int *startWorld, int nRows
 
         // create the next world state
         int *wholeNewWorld = malloc(sizeof(int) * nRows * nCols);
-        if (wholeNewWorld == NULL)
-        {
-            if (inv != NULL)
-            {
+        if (wholeNewWorld == NULL) {
+            if (inv != NULL) {
                 free(inv);
             }
             free(world);
             return -1;
         }
-        for(int t = 0; t < nThreads; t++){
+        for (int t = 0; t < nThreads; t++) {
             threadData[t].currWorld = world;
             threadData[t].invaders = inv;
             threadData[t].nCols = nCols;
@@ -267,36 +242,33 @@ int goi_pthread(int nThreads, int nGenerations, const int *startWorld, int nRows
         }
 
         //create threads
-        for(int t = 0; t < nThreads; t++){
+        for (int t = 0; t < nThreads; t++) {
 
             rc = pthread_create(&threads[t], NULL, thread_task, (void *) &threadData[t]);
-            if (rc){
+            if (rc) {
                 printf("Fail to create thread");
                 exit(-1);
 
             }
         }
 
-        for(int t = 0; t < nThreads; t++){
+        for (int t = 0; t < nThreads; t++) {
 
             pthread_join(threads[t], NULL);
-            deathToll+= threadData[t].numDeath;
+            deathToll += threadData[t].numDeath;
         }
 
-       //  get new states for each cell
-        for (int row = 0; row < nRows; row++)
-        {
-            for (int col = 0; col < nCols; col++)
-            {
-                int thread_num = ( row * nCols + col  ) % nThreads;
+        //  get new states for each cell
+        for (int row = 0; row < nRows; row++) {
+            for (int col = 0; col < nCols; col++) {
+                int thread_num = (row * nCols + col) % nThreads;
                 int nextState = threadData[thread_num].nextstate[threadData[thread_num].numRead];
                 threadData[thread_num].numRead += 1;
                 setValueAt(wholeNewWorld, nRows, nCols, row, col, nextState);
             }
         }
 
-        if (inv != NULL)
-        {
+        if (inv != NULL) {
             free(inv);
         }
 
