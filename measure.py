d824048f13dece07fc3ce1e9f69572153bbb4811
James Nolan <vip17jamesnolan@gmail.com>
2021-09-27 01:04:52 +0800

    update

--- measure.py
+++ measure.py
@@ -17,11 +17,11 @@ for sample_id in range(0, 7):
     result_omp[sample_id] = {}
 
     for thread_number in range(1, 65, 8):
-        T1 = time.time()
-        os.system(f"./goi_threads.out {path}/sample_inputs/sample{sample_id}.in test.out {thread_number}")
-        T2 = time.time()
-        time_taken_ms = (T2 - T1) * 1000
-        result_pthread[sample_id][thread_number] = time_taken_ms
+        #T1 = time.time()
+        #os.system(f"./goi_threads.out {path}/sample_inputs/sample{sample_id}.in test.out {thread_number}")
+        #T2 = time.time()
+        #time_taken_ms = (T2 - T1) * 1000
+        #result_pthread[sample_id][thread_number] = time_taken_ms
 
         T1 = time.time()
         os.system(f"./goi_omp.out {path}/sample_inputs/sample{sample_id}.in test.out {thread_number}")
@@ -32,5 +32,5 @@ print("Normal")
 print(str(result_normal))
 print("omp")
 print(str(result_omp))
-print("pthread")
-print(str(result_pthread))
+#print("pthread")
+#print(str(result_pthread))
