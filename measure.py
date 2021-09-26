21f75cef00a477e2992942b176ca0c69039e0b82
James Nolan <vip17jamesnolan@gmail.com>
2021-09-27 00:42:20 +0800

    mod

--- measure.py
+++ measure.py
@@ -8,7 +8,7 @@ print(path)
 result_normal = []
 result_omp = {}
 result_pthread = {}
-for sample_id in range(0, 3):
+for sample_id in range(0, 7):
     # Test sequential program
     T1 = time.time()
     os.system(f"./goi {path}/sample_inputs/sample{sample_id}.in test.out 1")
@@ -18,7 +18,7 @@ for sample_id in range(0, 3):
     result_pthread[sample_id] = {}
     result_omp[sample_id] = {}
 
-    for thread_number in range(1, 4, 1):
+    for thread_number in range(1, 65, 8):
         T1 = time.time()
         os.system(f"./goi_threads {path}/sample_inputs/sample{sample_id}.in test.out {thread_number}")
         T2 = time.time()
