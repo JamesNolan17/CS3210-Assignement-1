4984cd62574673c714e2a38a67e6caeb1a9d1ca5
James Nolan <vip17jamesnolan@gmail.com>
2021-09-27 00:56:44 +0800

    complied

--- measure.py
+++ measure.py
@@ -2,8 +2,6 @@ import os
 import time
 
 path = os.getcwd()
-os.system("make clean")
-os.system("make build")
 print(path)
 result_normal = []
 result_omp = {}
@@ -11,7 +9,7 @@ result_pthread = {}
 for sample_id in range(0, 7):
     # Test sequential program
     T1 = time.time()
-    os.system(f"./goi {path}/sample_inputs/sample{sample_id}.in test.out 1")
+    os.system(f"./goi.out {path}/sample_inputs/sample{sample_id}.in test.out 1")
     T2 = time.time()
     time_taken_ms = (T2 - T1) * 1000
     result_normal.append(time_taken_ms)
@@ -20,13 +18,13 @@ for sample_id in range(0, 7):
 
     for thread_number in range(1, 65, 8):
         T1 = time.time()
-        os.system(f"./goi_threads {path}/sample_inputs/sample{sample_id}.in test.out {thread_number}")
+        os.system(f"./goi_threads.out {path}/sample_inputs/sample{sample_id}.in test.out {thread_number}")
         T2 = time.time()
         time_taken_ms = (T2 - T1) * 1000
         result_pthread[sample_id][thread_number] = time_taken_ms
 
         T1 = time.time()
-        os.system(f"./goi_omp {path}/sample_inputs/sample{sample_id}.in test.out {thread_number}")
+        os.system(f"./goi_omp.out {path}/sample_inputs/sample{sample_id}.in test.out {thread_number}")
         T2 = time.time()
         time_taken_ms = (T2 - T1) * 1000
         result_omp[sample_id][thread_number] = time_taken_ms
